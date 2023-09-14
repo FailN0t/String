5aa1c5d2d34570e53dda6d74a415f52ddb1295cb
Igor Putyajkin <putyajkin99@gmail.com>
2023-09-15 01:51:52 +0300

    9.4

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,6 +7,13 @@ public class Main {
         String name = "Анна";
         String personInfo = name + " - " + age + " лет";
         String value = age.toString();
-        System.out.println(value);
+        System.out.println(String.valueOf(age));
+        System.out.println(Charset.defaultCharset());
+
+        String text = "Hello всем";
+        String encoded = new String(text.getBytes(),
+                StandardCharsets.US_ASCII); //создаём новую строку
+        System.out.println(encoded);
+        System.out.println(Charset.defaultCharset());
     }
 }
