60a442c6c0d2664636d77038d26aa57c71000398
Igor Putyajkin <putyajkin99@gmail.com>
2023-09-15 05:31:57 +0300

    9.5 join

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,19 @@
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import java.util.StringJoiner;
 
 public class Main {
     public static void main(String[] args) {
-        String header1 = "Content : text/html; charset=utf-8;";
-        String header2 = "Content : text/html; charset=windows-1251;";
-        String header3 = "Content : text/html; charset=ISO-8859-1;";
+        String name1 = "Василий";
+        String name2 = "Георгий";
+        String name3 = "Алексей";
 
-        String header4 = "Content : text/html;";
-        String header5 = "Content : text/html; charset=";
-        String header6 = "Content : text/html; charset=;";
+        StringJoiner joiner = new StringJoiner(", ");
+        joiner.add(name1);
+        joiner.add(name2);
+        joiner.add(name3);
+        System.out.println(joiner);
 
-        System.out.println(getEncoding(header1));
-        System.out.println(getEncoding(header2));
-        System.out.println(getEncoding(header3));
-
-        System.out.println(getEncoding(header4));
-        System.out.println(getEncoding(header5));
-        System.out.println(getEncoding(header6));
+        String list = String.join(", ", name1, name2, name3);
+        System.out.println(list);
     }
 
-    public static String getEncoding(String header){
-        String charsetStr = "charset=";
-        int start = header.indexOf(charsetStr);
-        int end = header.indexOf(';', start);
-
-        if(start<0 || end < 0){
-            return "";
-        }
-
-        String encoding = header.substring(start + charsetStr.length(), end);
-        return encoding;
-    }
 }
