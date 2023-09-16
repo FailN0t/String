b42a13b7fa08276b46bbd0a8001a06b9cfdbf4f7
Igor Putyajkin <putyajkin99@gmail.com>
2023-09-17 01:04:35 +0300

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class Main {
     public static void main(String[] args) {
-        String name = "Максим";
-        int birthYear = 1986;
-        String template = "{\"name\" : %s\", \"birthYear\": \"%d\"}";//%s строка %d целое число %f не целое
-        //%b boolean %n перенос строки %t дата Date %% символ процента
-        String result = String.format(template, name, birthYear);
-        System.out.println(result);
-        System.out.printf(template, name, birthYear);
+        String name1 = "Алексей";
+        String name2 = "Дмитрий";
+        System.out.println(name2 == name1);//ссылки на разные оъекты
+
+        name1 = "Алексей";
+        name2 = "Алексей";
+        System.out.println(name2 == name1);//ссылки на один оъект
+
+        name1 = "Алексей";
+        name2 = new String("Алексей");
+        System.out.println(name2 == name1);//ссылки на разные оъекты
+
+        name1 = "Алексей";
+        name2 = new String("Алексей").intern();//говорим передать ссылку на такойже объект
+        // в пуле
+        System.out.println(name2 == name1);//ссылки на разные оъекты
+
+
+
+
     }
 
 }
